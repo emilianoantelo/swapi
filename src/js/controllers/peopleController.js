ade5b5610e87490b017b18f46cf86605a47f5eff
Emiliano Antelo <emiliano.antelo@gmail.com>
2019-10-17 19:33:27 -0300

    lista de personajes

--- src/js/controllers/peopleController.js
+++ src/js/controllers/peopleController.js
@@ -1,50 +1,59 @@
 
 function getData2(_url, _showResults, _showError) {
   $.ajax(_url)
-    .done(function(data) {
+    .done(function (data) {
       _showResults(data);
     })
-    .fail(function(error) {
+    .fail(function (error) {
       _showError(error);
     });
 }
+var id = 1;
 function showResults(_peopleData) {
   if (_peopleData.results) {
-    var id = 1;
+
     var results = _peopleData.results;
+    console.log(_peopleData);
     for (var i = 0; i < results.length; i++) {
-      console.log(results[i]);
+      //console.log(results[i]);
       $("#personajes").append(
         "<tr><td>" +
-          i +
-          "</td> <td>" +
-          results[i].name +
-          "</td> <td>" +
-          results[i].gender +
-          "</td> <td>" +
-          results[i].height +
-          "</td> <td>" +
-          results[i].mass +
-          "</td> <td>" +
-          results[i].eye_color +
-          "</td> <td><button>" +
-          "Guardar" +
-          "</button></td> </tr>"
+        id +
+        "</td> <td>" +
+        results[i].name +
+        "</td> <td>" +
+        results[i].gender +
+        "</td> <td>" +
+        results[i].height +
+        "</td> <td>" +
+        results[i].mass +
+        "</td> <td>" +
+        results[i].eye_color +
+        "</td> <td><button>" +
+        "Guardar" +
+        "</button></td> </tr>"
+
       );
+      id++;
     }
   }
-  if (_peopleData.next) {
-    getData2(_peopleData.next, showResults, showError);
-  }
+  //if (_peopleData.next) {
+    //getData2(_peopleData.next, showResults, showError);
+  //}
+  console.log($('#seeMore'))
+  $('#seeMore').off().click(function () {
+    console.log('funciono')
+    getData2(_peopleData.next, showResults, showError)
+  
+  })
 }
 function showError(_error) {
   console.log(_error);
 }
 
+function peopleController() {
+  console.log('Se cargo la home')
+  getData2("https://swapi.co/api/people/", showResults, showError);
+}
 
-function peopleController(){
-    console.log('Se cargo la home')
-    getData2("https://swapi.co/api/people/", showResults, showError);
-  }
-  
-  export default peopleController
+export default peopleController
