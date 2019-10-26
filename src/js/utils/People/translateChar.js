5d681e38b2e4c217594f31cdd4acef4d0abbc29c
Emiliano Antelo <emiliano.antelo@gmail.com>
2019-10-26 10:45:19 -0300

    traduccion completa

--- src/js/utils/People/translateChar.js
+++ src/js/utils/People/translateChar.js
@@ -18,14 +18,50 @@ export default function translateChar(element) {
     } else {
         element.height += "cm"
     }
-    // switch (element.eye_color) {
-    //     case 'red':
-    //         element.eye_color = 'rojo'
-    //         break;
-    //     default:
-    //         element.eye_color = 'sinverguenza'
-    //         break;
-    // }
-
+     switch (element.eye_color) {
+         case 'red':
+             element.eye_color = 'rojo'
+             break;
+             case 'yellow':
+                element.eye_color = 'amarillo'
+                break;
+        case 'blue':
+             element.eye_color = 'azul'
+                    break;
+        case 'brown':
+            element.eye_color = 'marron'
+                     break;
+        case 'black':
+            element.eye_color = 'negro'
+                    break;
+        case 'orange':
+            element.eye_color = 'naranja'
+                    break;
+        case 'white':
+            element.eye_color = 'blanco'
+                    break;
+        case 'gold':
+            element.eye_color = 'dorado'
+                    break;
+        case 'hazel':
+            element.eye_color = 'avellana'
+                    break; 
+        case 'dark':
+            element.eye_color = 'oscuro'
+                    break;
+        case 'blue-gray':
+            element.eye_color = 'azul-gris'
+                    break;
+        case 'red, blue':
+            element.eye_color = 'rojo, azul'
+                    break;            
+        case 'green, yellow':
+            element.eye_color = 'verde,amarillo'
+                    break;            
+             default:
+             element.eye_color = 'desconocido'
+             break;
+     }
+    
 
 }
